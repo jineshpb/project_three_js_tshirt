e13b6f2a248bd7c61d0a24762944e57498c882db
jineshpb <89720519+jineshpb@users.noreply.github.com>
2023-08-17 16:35:46 +0530

    remove OPENAI key from env file

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,7 +60,7 @@ const Customizer = () => {
 
       setGeneratingImg(true)
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://project-threejs-tshirt-customizer.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
